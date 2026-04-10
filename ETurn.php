3025ac2dee4ce154aab8c3eb0c82199e3246a752
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-04-10 12:16:49 +0200

    feat: add ETurn class

---
+++ ETurn.php
@@ -0,0 +1,24 @@
+<?php
+
+class ETurn extends Turn {
+
+    private string $type = 'E';
+    private int $number = 0;
+    private static int $totalTurns = 0;
+
+    public function __construct()
+    {
+        $this->addNewTurn();
+        $this->number = self::$totalTurns;
+    }
+
+    public function addNewTurn()
+    {
+        self::$totalTurns++;
+    }
+
+    public function __toString(): string
+    {
+        return $this->type . $this->number;
+    }
+}
