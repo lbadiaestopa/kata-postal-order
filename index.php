7e1a7cc3c11370d8d061edfa1aae1c91925f0905
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-04-10 11:55:45 +0200

    feat: add basic html structure

--- index.php
+++ index.php
@@ -5,9 +5,28 @@ require_once __DIR__ . '/CTurn.php';
 require_once __DIR__ . '/TurnManager.php';
 
 $turnManager = new TurnManager();
-$turnManager->createNewTurn();
-$turnManager->createNewTurn();
-$turnManager->createNewTurn();
 
-$turnManager->callTurn();
-$turnManager->callTurn();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST">
+    <button type="submit" name="action" value="generateCTurn">
+        Cita prèvia
+    </button>
+</form>
+<?php if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($_POST['action'] === 'generateCTurn') {
+        $turnManager->createNewTurn();
+    }
+}
+?>
+</body>
+</html>
+
