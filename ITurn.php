98763bad1fc88c77c3219b1d454c783fd16f52f0
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-04-10 17:28:47 +0200

    feat: add classes for all turn types

---
+++ ITurn.php
@@ -0,0 +1,24 @@
+<?php
+
+class ITurn extends Turn {
+
+    private string $type = 'I';
+    private int $number = 0;
+    private static int $totalTurns = 0;
+
+    public function __construct()
+    {
+        $this->addNewTurn();
+        $this->number = self::$totalTurns;
+    }
+
+    public function addNewTurn()
+    {
+        self::$totalTurns++;
+    }
+
+    public function __toString(): string
+    {
+        return $this->type . $this->number;
+    }
+}
---
+++ RTurn.php
@@ -0,0 +1,24 @@
+<?php
+
+class RTurn extends Turn {
+
+    private string $type = 'R';
+    private int $number = 0;
+    private static int $totalTurns = 0;
+
+    public function __construct()
+    {
+        $this->addNewTurn();
+        $this->number = self::$totalTurns;
+    }
+
+    public function addNewTurn()
+    {
+        self::$totalTurns++;
+    }
+
+    public function __toString(): string
+    {
+        return $this->type . $this->number;
+    }
+}
