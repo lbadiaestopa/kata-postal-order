b9c64bd534184a055b74b2697e5cbf1b09446a65
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-04-10 11:34:08 +0200

    feat: added new turns and called turns for testing

--- index.php
+++ index.php
@@ -2,17 +2,12 @@
 
 require_once __DIR__ . '/Turn.php';
 require_once __DIR__ . '/CTurn.php';
-require_once __DIR__ . '/TurnGenerator.php';
+require_once __DIR__ . '/TurnManager.php';
 
-/*$turn1 = new CTurn();
-$turn2 = new CTurn();
-$turn3 = new CTurn();
+$turnManager = new TurnManager();
+$turnManager->newTurn();
+$turnManager->newTurn();
+$turnManager->newTurn();
 
-echo $turn1 . PHP_EOL;
-echo $turn2 . PHP_EOL;
-echo $turn3 . PHP_EOL;*/
-
-$turn = new TurnGenerator();
-$turn->newTurn();
-
-echo $turn;
+$turnManager->callTurn();
+$turnManager->callTurn();
